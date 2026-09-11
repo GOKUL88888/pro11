95630e9b045fc07910a6023c693ba618df450314
GOKUL88888 <naughtyboy3101@gmail.com>
2026-09-11 12:19:48 +0530

    Update mount_uuid.sh

--- mount_uuid.sh
+++ mount_uuid.sh
@@ -2,27 +2,25 @@
 
 # ==========================================
 # Mount Filesystem Using UUID
-# Student Name:
-# Roll Number:
+# Student Name:gokul s
+# Roll Number:1u24it036
 # ==========================================
 
-
 # Display filesystem UUID
-
-
+blkid
 
 
 # Create mount directory
-
-
+mkdir -p /mnt/mydisk
 
 
 # Mount filesystem using UUID
 # Replace YOUR_UUID with actual UUID
-
-
+mount UUID=YOUR_UUID /mnt/mydisk
 
 
 # Display mounted filesystem
+df -h /mnt/mydisk
 
+exit 0
 
